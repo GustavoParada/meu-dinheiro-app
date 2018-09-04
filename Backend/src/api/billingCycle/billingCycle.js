e7d6ad8b3735cc8cd9fe3df293334629d4ad4170
Gustavo <gustavoparada.net@gmail.com>
2018-09-03 21:59:30 -0300

    rotas count

--- Backend/src/api/billingCycle/billingCycle.js
+++ Backend/src/api/billingCycle/billingCycle.js
@@ -1,16 +1,16 @@
 const restful = require('node-restful')
 const mongoose = restful.mongoose
 const creditSchema = new mongoose.Schema({
-    nome: { type: String, required: true },
+    name: { type: String, required: true },
     value: { type: Number, min: 0, required: true },
 
 })
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito'] },
     status: {
-        type: Number, required: false,
+        type: String, required: false,
         uppercase: true, enum: ['PAGO', 'PRENDENTE', 'AGENDADO']
     }
 })
