3e08fd6e8c9cfea85a727241efe968fce01d4363
Gustavo <gustavoparada.net@gmail.com>
2018-10-30 19:06:54 -0300

    Rotas, menus, e componentes.

---
+++ Frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props =>(
+    <footer className='main-footer'>
+    <strong>
+        Copyrigth &copy; 2018
+        <a href='http://www.gustavoparada.com.br' target='_blanc'>GUPA desenvolvimento</a> 
+    </strong>
+
+    </footer>
+)
