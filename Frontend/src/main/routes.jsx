a08c7e0ed844a55ac7019db3bf2ce0ff708f7e84
Gustavo <gustavoparada.net@gmail.com>
2018-10-31 16:28:36 -0300

    checkpoint de dashborad terminado e opção sem redux para estudo

--- Frontend/src/main/routes.jsx
+++ Frontend/src/main/routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 
-import Dashboard from '../dashBoard/dashboard'
+import Dashboard from '../dashBoard2/dashboard2'
 import BillingCycle from '../billingCycle/billingCycle'
 
 export default props => (
